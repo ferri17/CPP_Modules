e7d80ab479cfb8c55565ad4a4eaf34a0969902fd
Ferran <ferranbosch17@gmail.com>
2023-09-27 01:32:14 +0200

    First logs ex02

--- CPP_Module_00/ex02/Account.cpp
+++ CPP_Module_00/ex02/Account.cpp
@@ -6,8 +6,34 @@
 /*   By: fbosch <fbosch@student.42barcelona.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/27 01:00:50 by fbosch            #+#    #+#             */
-/*   Updated: 2023/09/27 01:02:34 by fbosch           ###   ########.fr       */
+/*   Updated: 2023/09/27 01:31:25 by fbosch           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Account.hpp"
+#include <iostream>
+#include <ctime>
+#include <iomanip>
+
+Account::Account( void ) {
+
+	return ;
+}
+
+Account::~Account( void ) {
+
+	return ;
+}
+
+Account::Account( int initial_deposit ) {
+
+	static int	index = 0;
+	std::time_t now = std::time(nullptr);
+    std::tm* time_info = std::localtime(&now);
+
+    std::cout << std::put_time(time_info, "[%Y%m%d_%H%M%S] ");
+	std::cout << "index:" << index << ";";
+	std::cout << "amount:" << initial_deposit << ";";
+	std::cout << "created" << std::endl;
+	index++;
+}
--- CPP_Module_00/ex02/tests.cpp
+++ CPP_Module_00/ex02/tests.cpp
@@ -19,10 +19,10 @@ int		main( void ) {
 	typedef std::vector<int>								  ints_t;
 	typedef std::pair<accounts_t::iterator, ints_t::iterator> acc_int_t;
 
-	/* int	const				amounts[]	= { 42, 54, 957, 432, 1234, 0, 754, 16576 };
+	int	const				amounts[]	= { 42, 54, 957, 432, 1234, 0, 754, 16576 };
 	size_t const			amounts_size( sizeof(amounts) / sizeof(int) );
 	accounts_t				accounts( amounts, amounts + amounts_size );
-	accounts_t::iterator	acc_begin	= accounts.begin();
+	/*accounts_t::iterator	acc_begin	= accounts.begin();
 	accounts_t::iterator	acc_end		= accounts.end();
 
 	int	const			d[]			= { 5, 765, 564, 2, 87, 23, 9, 20 };
