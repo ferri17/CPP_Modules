f57ae87ddbe94ba14efee71ab98a1a35fa7d091a
Ferran <ferranbosch17@gmail.com>
2024-02-15 01:06:02 +0100

    CPP08 ex01 half

---
+++ CPP_Module_08/ex01/Span.hpp
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Span.hpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fbosch <fbosch@student.42barcelona.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/03 22:26:25 by fbosch            #+#    #+#             */
+/*   Updated: 2024/02/15 01:01:42 by fbosch           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+
+#include <iostream>
+#include <limits>
+#include <algorithm>
+#include <vector>
+#include <cstdlib>
+
+class	Span
+{
+	private:
+		std::vector<int>	_numbers;
+		unsigned int		_size;
+	public:
+		Span(void);
+		Span(unsigned int size);
+		Span(const Span & other);
+		Span &	operator=(const Span & other);
+		~Span(void);
+
+		void			addNumber(int nb);
+		long int		shortestSpan(void);
+		long int		longestSpan(void);
+};
---
+++ CPP_Module_08/ex01/main.cpp
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fbosch <fbosch@student.42barcelona.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/30 00:45:29 by fbosch            #+#    #+#             */
+/*   Updated: 2024/02/15 01:05:21 by fbosch           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Span.hpp"
+
+int	main(void)
+{
+	Span sp = Span(5);
+	
+	sp.addNumber(6);
+	sp.addNumber(3);
+	sp.addNumber(17);
+	sp.addNumber(9);
+	sp.addNumber(11);
+	//std::cout << sp.shortestSpan() << std::endl;
+	std::cout << sp.longestSpan() << std::endl;
+	return 0;
+}
